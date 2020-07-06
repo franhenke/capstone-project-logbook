fa9acaabe21e119e7d10458fcb28f935b6e72b91
franhenke <franci.henke@gmail.com>
2020-07-06 16:30:22 +0200

    add GlobalStyle

---
+++ src/components/Datepicker.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+export default function Datepicker() {
+  const [date, setDate] = useState(new Date())
+  const handleChange = (date) => setDate(date)
+  const today = new Date()
+  let last7Days = new Date()
+  last7Days.setDate(last7Days.getDate() - 7)
+
+  return (
+    <DatePicker
+      selected={date}
+      onChange={handleChange}
+      minDate={last7Days}
+      maxDate={today}
+      dateFormat="d MMMM, yyyy"
+      placeholderText="Click to select a date"
+    />
+  )
+}
---
+++ src/components/Datepicker.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Datepicker from './Datepicker'
+
+export default {
+  title: 'Datepicker',
+  component: Datepicker,
+}
+
+export const DatepickerPreview = () => {
+  return <Datepicker />
+}
