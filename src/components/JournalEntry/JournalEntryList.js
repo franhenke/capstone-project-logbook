e8624e43a864cb1e5f50826714704ff110eaf3af
franhenke <franci.henke@gmail.com>
2020-07-10 10:24:14 +0200

    Add dayjs and format date to DDMMYYYY

--- src/components/JournalEntry/JournalEntryList.js
+++ src/components/JournalEntry/JournalEntryList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import dayjs from 'dayjs'
 
 export default function JournalEntryList({ entry }) {
+  const parsedDate = dayjs(entry.values.date)
   return (
     <EntryStyled>
-      <p>{entry.values.date}</p>
+      <p>{parsedDate.format('DD.MM.YYYY')}</p>
       <p>{entry.values.city}</p>
       <p>{entry.values.caption}</p>
       <p>{entry.values.memory}</p>
