7e3d6fe9e5b420177868fc2a65066d305da0ed33
franhenke <franci.henke@gmail.com>
2020-07-12 19:28:16 +0200

    Add Welcome.js

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import JournalForm from '../JournalForm/JournalForm'
+import styled from 'styled-components'
+import { animated, useTransition } from 'react-spring'
+import BG-image from '../../images/background.jpg'
+export default function Welcome() {
+
+return (<>
+
+<BackgroundStyled>
+<WelcomeStyled>Welcome Back, Franci</WelcomeStyled>
+<LinkStyled to="/journalform">Go</LinkStyled>
+</BackgroundStyled>
+
+</>
+
+)
+
+}
+
+const BackgroundStyled = styled.div`
+background-image: url(${Bg-image});
+height: 100vh;
+`
+const WelcomeStyled = styled.h1`
+font-size: 20px;
+color: white;
+text-align: right;
+`
+const LinkStyled = styled.a`
+text-decoration: none;
+font-size: 18px;
+`
