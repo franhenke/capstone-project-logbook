56f08d45f0fe456df22fbb2f424883d1a57ae6b7
franhenke <franci.henke@gmail.com>
2020-07-15 10:34:06 +0200

    remove unused files

--- src/components/User/User.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { animated, useTransition } from 'react-spring'
-
-export default function User() {
-  return <H1>User</H1>
-}
