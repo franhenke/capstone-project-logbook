be6fd7c8d13daa4500ba5c275868238abddcd904
francihenke <franci.henke@gmail.com>
2020-07-23 16:56:04 +0200

    Create and define routes for later use

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+export const HOME = '/home'
+export const JOURNALDETAILS = '/home/journalentry/:entryId'
+export const JOURNALFORM = '/home/JournalForm'
+
+export const WELCOME = '/'
+export const SIGN_IN = '/login'
+export const SIGN_UP = '/register'
