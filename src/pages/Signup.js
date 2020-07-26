ee431097daaf505d15a1a358069babdbb73aa9b4
francihenke <franci.henke@gmail.com>
2020-07-26 13:25:56 +0200

    Add styling to signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import Button from '../components/Button/Button'
 
@@ -15,7 +15,6 @@ export default function SignUp({ setProfile, signUp }) {
     register,
     handleSubmit,
     errors,
-    setError,
     triggerValidation,
     getValues,
     formState,
@@ -30,102 +29,112 @@ export default function SignUp({ setProfile, signUp }) {
     }
   }
 
-  const history = useHistory()
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <div className="name">
-        <input type="text" name="name" ref={register({ required: true })} />
-      </div>
-      <div className="email">
-        <input
-          type="email"
-          name="email"
-          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-        />
-        {((errors.email && errors.email.type === 'required') ||
-          (errors.email && errors.email.type === 'pattern')) && (
+      <label>Email</label>
+      <InputStyled
+        type="email"
+        name="email"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+      />
+      {((errors.email && errors.email.type === 'required') ||
+        (errors.email && errors.email.type === 'pattern')) && (
           <Error>Please check your entered e-mail address</Error>
         )}
-        {errors.email && errors.email.type === 'inUse' && (
-          <Error>{errors.email.message}</Error>
-        )}
-      </div>
-      <div className="password">
-        <input
-          ref={register({
-            required: true,
-            minLength: {
-              value: 6,
-              message: 'Password must have at least 6 characters',
-            },
-          })}
-          onChange={validateRepeat}
-          type="password"
-          name="password"
-        />
-        {errors.password && (
-          <Error>Password must have at least 6 characters</Error>
-        )}
-      </div>
-      <div>
-        <input
-          name="passwordRepeat"
-          type="password"
-          ref={register({ required: true, validate: repeatValidation })}
-        />
-        {errors.passwordRepeat && (
-          <Error>{errors.passwordRepeat.message}</Error>
-        )}
-        <div className="signUp">
-          <Button text="Sign up" type="submit" />
-        </div>
-        <Link to="/login">
-          <p>Back to login</p>
-        </Link>
-      </div>
-    </Form>
+
+
+      <label>Password</label>
+      <InputStyled
+        ref={register({
+          required: true,
+          minLength: {
+            value: 6,
+            message: 'Password must have at least 6 characters',
+          },
+        })}
+        onChange={validateRepeat}
+        type="password"
+        name="password"
+      />
+
+      {errors.password && (
+        <Error>'Password must have at least 6 characters'</Error>
+      )}
+
+      <label>
+        Please repeat your password</label>
+      <InputStyled
+        name="passwordRepeat"
+        type="password"
+        ref={register({ required: true, validate: repeatValidation })}
+      />
+
+      {errors.passwordRepeat && <Error>{errors.passwordRepeat.message}</Error>}
+
+      <Button text="Sign up" type="submit" />
+
+      <Link to="/login">
+        <p>Back to login</p>
+      </Link>
+    </Form >
   )
 
   function onSubmit(data) {
     setProfile(data)
     signUp(data)
-      .then((res) => {
-        if (res.code === 'auth/email-already-in-use') {
-          return setError('email', 'inUse', 'E-mail address already in use')
-        }
-        setTimeout(history.push('/'), 3000)
-      })
-      .catch((error) => {
-        console.log(
-          'Sorry, there was an error with the server. Please try again later.',
-          error
-        )
-      })
+    //     .then((res) => {
+    //       if (res.code === 'auth/email-already-in-use') {
+    //         return setError('email', 'inUse', 'E-mail address already in use')
+    //       }
+    //       setTimeout(history.push('/'), 2000)
+    //       alert('You are now registered')
+    //     })
+    //     .catch((error) => {
+    //       console.log(
+    //         'Sorry, there was an error with the server. Please try again later.',
+    //         error
+    //       )
+    //     })
+    // }
   }
 }
 
 const Form = styled.form`
+  font-family: Roboto;
   display: flex;
   flex-direction: column;
   margin: 80px 20px 20px;
-
-  div {
-    margin: 24px 0;
-    width: 100%;
+  label {
+    color: #21374f;
+    font-size: 16px;
+    letter-spacing: 2px;
+    height: 16px;
+    opacity: 0.5;
+    margin: 25px 0 10px;
   }
 
-  p {
-    text-align: center;
-  }
+  `
+const InputStyled = styled.input` 
+  background: var(--background);
+  color: var(--text);
+  outline: none;
+  border: none;
+  border-bottom-style: solid;
+  border-bottom-color: #979797;
+  border-bottom-width: 1px;
+  font-size: 16px;
+  opacity: 0.5;
+  width: 285px;
 
-  .signup {
-    margin-top: 44px;
-    text-align: center;
-    button {
-      padding: 16px 60px;
+  &:focus {
+      border: none;
+      border-bottom-style: solid;
+      border-bottom-color: var(--highlight);
+      border-bottom-width: 1px;
     }
-  }
-`
+  `
+
 const Error = styled.p`
-  color: red;
+color: red;
 `
+
