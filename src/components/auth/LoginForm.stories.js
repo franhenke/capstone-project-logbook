0473eabc264495fd1e43df8580c99297e5463306
franhenke <franci.henke@gmail.com>
2020-07-30 16:41:42 +0200

    Delete unused components

--- src/components/auth/LoginForm.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from './Login'
-
-export default {
-  title: 'Login Form',
-  component: Login,
-}
-
-export const LoginForm = () => <Login />
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-export const JOURNALENTRIES = '/home'
-export const JOURNALDETAILS = '/home/journalentry/:entryId'
-export const JOURNALFORM = '/home/JournalForm'
-
-export const WELCOME = '/'
-export const LOGIN = '/login'
-export const REGISTER = '/register'
