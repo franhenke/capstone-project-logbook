fbd74df9b063dda83f4479e1d9dd6b1b4b4547ab
franhenke <franci.henke@gmail.com>
2020-08-17 00:57:35 +0200

    Move Register to different folder

--- src/pages/Register.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Register from './Register'
-
-export default {
-  title: 'Register Form',
-  component: Register,
-}
-
-export const SignUpForm = () => <Register />
