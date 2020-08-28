e615c23fe54020c48c4b4bb131d756979a1513bb
franhenke <franci.henke@gmail.com>
2020-08-28 15:13:36 +0200

    Add WelcomeMessage depending on time of day

--- src/components/UI/WelcomeMessage.js
+++ src/components/UI/WelcomeMessage.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import LoginContext from '../../services/auth/LoginContext'
 
 export default function WelcomeMessage() {
   const { user } = useContext(LoginContext)
+  const [hour, setHour] = useState(new Date().toLocaleTimeString())
 
-  return (
-    <WelcomeMessageStyled>
-      Hello {user.displayName}, let's create memories!{' '}
-    </WelcomeMessageStyled>
-  )
+  let greeting
+  if (hour < 12) {
+    greeting = `Good Morning, ${user.displayName}`
+  }
+  if (hour > 12 && hour < 16) {
+    greeting = `Good Afternoon, ${user.displayName}`
+  } else {
+    greeting = `Good Evening, ${user.displayName}`
+  }
+
+  useEffect(() => {
+    let time = setInterval(() => {
+      setHour(new Date().getHours())
+    }, 0)
+
+    return () => clearInterval(time)
+  }, [])
+
+  return <WelcomeMessageStyled>{greeting}</WelcomeMessageStyled>
 }
-const WelcomeMessage = styled.h2`
-  font-size: 1em;
-  color: var(--text);
-  position: absolute;
-  top: 6em;
-  left: 4em;
+
+const WelcomeMessageStyled = styled.h3`
+  color: var(--mint);
+  text-align: center;
+  margin-top: 2em;
 `
